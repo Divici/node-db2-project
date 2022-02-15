ebc899157bd88ef44485e7bca360aa5fe78a68f5
david-aihe <doa9200@gmail.com>
2022-02-15 15:30:59 -0500

    created seeds file

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable('cars', tbl =>{
     tbl.increments();
-    tbl.text('vin').unique().notNullable();
-    tbl.text('make').notNullable();
-    tbl.text('model').notNullable();
-    tbl.integer('mileage').notNullable();
-    tbl.text('title').nullable();
-    tbl.text('transmission').nullable();
+    tbl.string('vin', 17).unique().notNullable();
+    tbl.string('make', 128).notNullable();
+    tbl.string('model', 128).notNullable();
+    tbl.integer('mileage').unsigned().notNullable();
+    tbl.string('title', 128).nullable();
+    tbl.string('transmission', 128).nullable();
   })
 };
 
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 151000,
+        title: 'clean',
+        transmission: 'automatic',
+    },
+    {
+        vin: '1111111111291',
+        make: 'mazda',
+        model: 'CX-5',
+        mileage: 29000,
+        transmission: 'automatic',
+    },
+    {
+        vin: '1111111111771',
+        make: 'tesla',
+        model: 'model-Y',
+        mileage: 72435,
+        title: 'clean',
+    },
+]
+
+exports.seed = function(knex) {
+    return knex('cars')
+        .truncate().then(()=>{
+            return knex('cars').insert(cars)
+        })  
+}
