ea58e5cccd4acc0142bef257079ee52d6e0fe531
david-aihe <doa9200@gmail.com>
2022-02-15 15:06:14 -0500

    Created cars-router requests

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,39 @@
-// DO YOUR MAGIC
+const CarsModel = require('./cars-model')
+const router = require('express').Router()
+const {checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique} =require('./cars-middleware')
+
+router.get('/', (req, res, next) =>{
+    CarsModel.getAll()
+        .then((cars) => {
+            res.json(cars)
+        })
+        .catch(next)
+})
+
+router.get('/:id', checkCarId, (req, res, next) =>{
+    res.json(req.car);
+})
+
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next) =>{
+    try {
+        const newCar = await CarsModel.create({
+            vin: req.body.vin,
+            make: req.body.make,
+            model: req.body.model,
+            mileage: req.body.mileage
+        })
+        res.status(201).json(newCar);
+    }
+    catch (err){
+        next();
+    }
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack
+    })
+  })
+  
+  module.exports = router;
